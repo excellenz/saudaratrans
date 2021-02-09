bde2e1b8f407944b38d45d5d7af308f1f0aae84a
Excellenz <63463920+jaruki2014@users.noreply.github.com>
2021-02-09 19:39:26 +0700

    finish - alhamdulillah

--- page/pariwisata.php
+++ page/pariwisata.php
@@ -2,68 +2,109 @@
 <div class="container">
   <div class="row">
     <div class="col-lg-12">
-      <h2 class="pageTitle">ARMADA Saudara Trans - Jenis Mobil, Harga dan Ketersediaan.</h2>
+      <h2 class="pageTitle">BIRO PARIWISATA SAUDARA TRANS</h2>
     </div>
   </div>
 </div>
 </section>
 <section id="content">
-<div class="container">
-  <div class="row">
-    <div class="col-lg-12">
-      <div class="clearfix">
+<div class="container">         
+<div class="about">
+  <div class="row"> 
+    <div class="col-md-4">
+      <div class="block-heading-two">
+        <h3><span>BIRO PARIWISATA Saudara Trans</span></h3>
       </div>
-      <div class="row">
-        <section id="projects">
-        <ul id="thumbs" class="portfolio">
-          <!-- Item Project and Filter Name -->
-          <li class="item-thumbs col-lg-3 design">
-          <!-- Fancybox - Gallery Enabled - Title - Full Image -->
-          <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Portfolio name" href="img/works/1.jpg">
-          <span class="overlay-img"></span>
-          <span class="overlay-img-thumb"><i class="icon-info-blocks fa fa-search"></i></span>
-          </a>
-          <!-- Thumb Image and Description -->
-          <img src="img/works/1.jpg" alt="">
-          </li>
-          <!-- End Item Project -->
-          <!-- Item Project and Filter Name -->
-          <li class="item-thumbs col-lg-3 design">
-          <!-- Fancybox - Gallery Enabled - Title - Full Image -->
-          <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Portfolio name" href="img/works/2.jpg">
-          <span class="overlay-img"></span>
-          <span class="overlay-img-thumb"><i class="icon-info-blocks fa fa-search"></i></span>
-          </a>
-          <!-- Thumb Image and Description -->
-          <img src="img/works/2.jpg" alt="">
-          </li>
-          <!-- End Item Project -->
-          <!-- Item Project and Filter Name -->
-          <li class="item-thumbs col-lg-3 design">
-          <!-- Fancybox - Gallery Enabled - Title - Full Image -->
-          <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Portfolio name" href="img/works/3.jpg">
-          <span class="overlay-img"></span>
-          <span class="overlay-img-thumb"><i class="icon-info-blocks fa fa-search"></i></span>
-          </a>
-          <!-- Thumb Image and Description -->
-          <img src="img/works/3.jpg" alt="">
-          </li>
-          <!-- End Item Project -->
-          <!-- Item Project and Filter Name -->
-          <li class="item-thumbs col-lg-3 design">
-          <!-- Fancybox - Gallery Enabled - Title - Full Image -->
-          <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Portfolio name" href="img/works/4.jpg">
-          <span class="overlay-img"></span>
-          <span class="overlay-img-thumb"><i class="icon-info-blocks fa fa-search"></i></span>
-          </a>
-          <!-- Thumb Image and Description -->
-          <img src="img/works/4.jpg" alt="">
-          </li>
-          <!-- End Item Project -->
-        </ul>
-        </section>
+      <p>Jasa pelayanan pariwisata seperti wisata santri, wisata sekolah, wisata pengajian islami dan wisata korporat ke tempat-tempat wisata atau tujuan tertentu sesuai dengan permintaan dari pelanggan.</p>
+    </div>
+    <div class="col-md-4">
+      <div class="block-heading-two">
+        <h3><span>TUJUAN wisata</span></h3>
+      </div>    
+      <div class="panel-group" id="accordion-alt3">
+        <div class="panel"> 
+         <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseOne-alt3">
+            <i class="fa fa-angle-right"></i> Jabodetabek
+            </a>
+          </h4>
+         </div>
+        </div>
+        <div class="panel">
+         <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTwo-alt3">
+            <i class="fa fa-angle-right"></i> Bandung dan sekitarnya
+            </a>
+          </h4>
+         </div>
+        </div>
+        <div class="panel">
+         <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseThree-alt3">
+            <i class="fa fa-angle-right"></i> Jawa Tengah dan sekitarnya
+            </a>
+          </h4>
+         </div>
+        </div>
+        <div class="panel">
+         <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseFour-alt3">
+            <i class="fa fa-angle-right"></i> Jawa Timur dan Bali
+            </a>
+          </h4>
+         </div>
+        </div>
+        <div class="panel">
+         <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseFour-alt3">
+            <i class="fa fa-angle-right"></i> Wisata religi (ziarah wali songo dll)
+            </a>
+          </h4>
+         </div>
+        </div>
       </div>
+      <!-- Accordion ends -->
+    </div>
+    <div class="col-md-4">
+      <div class="block-heading-two">
+        <h3><span>LAYANAN BIRO PARIWISATA</span></h3>
+      </div>                
+      <h6>Bus bersih, nyaman dan terawat</h6>
+      <div class="progress pb-sm">
+        <!-- White color (progress-bar-white) -->
+        <div class="progress-bar progress-bar-red" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+         <span class="sr-only">100% Complete (success)</span>
+        </div>
+      </div>
+      <h6>Tim EO Professional</h6>
+      <div class="progress pb-sm">
+        <div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+         <span class="sr-only">100% Complete (success)</span>
+        </div>
+      </div>
+      <h6>Hotel Nyaman</h6>
+      <div class="progress pb-sm">
+        <div class="progress-bar progress-bar-lblue" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+         <span class="sr-only">100% Complete (success)</span>
+        </div>
+      </div>
+      <h6>Rumah makan terjangkau</h6>
+      <div class="progress pb-sm">
+        <div class="progress-bar progress-bar-yellow" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+         <span class="sr-only">100% Complete (success)</span>
+        </div>
+       </div>
     </div>
   </div>
+  <div class="pricing-action">
+    <a href="https://api.whatsapp.com/send?phone=6285220489206.&text=Salam%20Saudara%20Trans.%20Saya%20ingin%20pesan%20biro%20pariwisata%20. . .%20" class="img-responsive"><img src="img/pesan.png"></a>
+  </div>
+</div>
 </div>
 </section>
+
--- page/travel.php
+++ page/travel.php
@@ -2,68 +2,100 @@
 <div class="container">
   <div class="row">
     <div class="col-lg-12">
-      <h2 class="pageTitle">ARMADA Saudara Trans - Jenis Mobil, Harga dan Ketersediaan.</h2>
+      <h2 class="pageTitle">TRAVEL SAUDARA TRANS</h2>
     </div>
   </div>
 </div>
 </section>
 <section id="content">
-<div class="container">
-  <div class="row">
-    <div class="col-lg-12">
-      <div class="clearfix">
+<div class="container">         
+<div class="about">
+  <div class="row"> 
+    <div class="col-md-4">
+      <div class="block-heading-two">
+        <h3><span>TRAVEL Saudara Trans</span></h3>
       </div>
-      <div class="row">
-        <section id="projects">
-        <ul id="thumbs" class="portfolio">
-          <!-- Item Project and Filter Name -->
-          <li class="item-thumbs col-lg-3 design">
-          <!-- Fancybox - Gallery Enabled - Title - Full Image -->
-          <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Portfolio name" href="img/works/1.jpg">
-          <span class="overlay-img"></span>
-          <span class="overlay-img-thumb"><i class="icon-info-blocks fa fa-search"></i></span>
-          </a>
-          <!-- Thumb Image and Description -->
-          <img src="img/works/1.jpg" alt="">
-          </li>
-          <!-- End Item Project -->
-          <!-- Item Project and Filter Name -->
-          <li class="item-thumbs col-lg-3 design">
-          <!-- Fancybox - Gallery Enabled - Title - Full Image -->
-          <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Portfolio name" href="img/works/2.jpg">
-          <span class="overlay-img"></span>
-          <span class="overlay-img-thumb"><i class="icon-info-blocks fa fa-search"></i></span>
-          </a>
-          <!-- Thumb Image and Description -->
-          <img src="img/works/2.jpg" alt="">
-          </li>
-          <!-- End Item Project -->
-          <!-- Item Project and Filter Name -->
-          <li class="item-thumbs col-lg-3 design">
-          <!-- Fancybox - Gallery Enabled - Title - Full Image -->
-          <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Portfolio name" href="img/works/3.jpg">
-          <span class="overlay-img"></span>
-          <span class="overlay-img-thumb"><i class="icon-info-blocks fa fa-search"></i></span>
-          </a>
-          <!-- Thumb Image and Description -->
-          <img src="img/works/3.jpg" alt="">
-          </li>
-          <!-- End Item Project -->
-          <!-- Item Project and Filter Name -->
-          <li class="item-thumbs col-lg-3 design">
-          <!-- Fancybox - Gallery Enabled - Title - Full Image -->
-          <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Portfolio name" href="img/works/4.jpg">
-          <span class="overlay-img"></span>
-          <span class="overlay-img-thumb"><i class="icon-info-blocks fa fa-search"></i></span>
-          </a>
-          <!-- Thumb Image and Description -->
-          <img src="img/works/4.jpg" alt="">
-          </li>
-          <!-- End Item Project -->
-        </ul>
-        </section>
+      <p>Layanan antar jemput stasiun dan bandara dengan armada lengkap yang bersih, nyaman dan terawat. Anda akan mendapatkan layanan antar dan jemput mobil, gratis soft drink dan diskon yang menarik.</p>
+    </div>
+    <div class="col-md-4">
+      <div class="block-heading-two">
+        <h3><span>TUJUAN TRAVEL</span></h3>
+      </div>    
+      <div class="panel-group" id="accordion-alt3">
+        <div class="panel"> 
+         <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseOne-alt3">
+            <i class="fa fa-angle-right"></i> Jakarta
+            </a>
+          </h4>
+         </div>
+        </div>
+        <div class="panel">
+         <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTwo-alt3">
+            <i class="fa fa-angle-right"></i> Depok, Bogor, Tanggerang
+            </a>
+          </h4>
+         </div>
+        </div>
+        <div class="panel">
+         <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseThree-alt3">
+            <i class="fa fa-angle-right"></i> Bandung
+            </a>
+          </h4>
+         </div>
+        </div>
+        <div class="panel">
+         <div class="panel-heading">
+          <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseFour-alt3">
+            <i class="fa fa-angle-right"></i> Jogjakarta dan Sekitarnya
+            </a>
+          </h4>
+         </div>
+        </div>
       </div>
+      <!-- Accordion ends -->
+    </div>
+    <div class="col-md-4">
+      <div class="block-heading-two">
+        <h3><span>LAYANAN TRAVEL</span></h3>
+      </div>                
+      <h6>Mobil bersih, nyaman dan terawat</h6>
+      <div class="progress pb-sm">
+        <!-- White color (progress-bar-white) -->
+        <div class="progress-bar progress-bar-red" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+         <span class="sr-only">100% Complete (success)</span>
+        </div>
+      </div>
+      <h6>Sopir ramah dan berpengalaman</h6>
+      <div class="progress pb-sm">
+        <div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+         <span class="sr-only">100% Complete (success)</span>
+        </div>
+      </div>
+      <h6>Antar jemput sampai tujuan</h6>
+      <div class="progress pb-sm">
+        <div class="progress-bar progress-bar-lblue" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+         <span class="sr-only">100% Complete (success)</span>
+        </div>
+      </div>
+      <h6>Gratis soft drink dan diskon menarik</h6>
+      <div class="progress pb-sm">
+        <div class="progress-bar progress-bar-yellow" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+         <span class="sr-only">100% Complete (success)</span>
+        </div>
+       </div>
     </div>
   </div>
+  <div class="pricing-action">
+    <a href="https://api.whatsapp.com/send?phone=6285220489206.&text=Salam%20Saudara%20Trans.%20Saya%20ingin%20pesan%20travel%20. . .%20" class="img-responsive"><img src="img/pesan.png"></a>
+  </div>
+</div>
 </div>
 </section>
+
