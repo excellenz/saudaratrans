81ea1a3ba670a5bbe075cd62154f0eab3155e136
Excellenz <63463920+jaruki2014@users.noreply.github.com>
2021-02-09 18:36:24 +0700

    pricing

--- page/home.php
+++ page/home.php
@@ -47,8 +47,197 @@
     </div>
   </div>
   </section>
+
   <section id="content">
-  
+  <div class="container">  
+    <!-- end divider -->
+    <div class="row">
+      <div class="col-lg-12">
+        <center><h2>HARGA RENTAL MOBIL DAN JASA DRIVER</h2></center>
+        <br>
+      </div>
+      <div class="col-lg-3">
+        <div class="pricing-box-item">
+          <div class="pricing-heading">
+            <h3><strong>Rental 12 Jam</strong></h3>
+          </div>
+          <div class="pricing-terms">
+            <h6>Rp. 200.000,-</h6>
+          </div>
+          <div class="pricing-container">
+            <ul>
+              <li><i class="icon-ok"></i> Semua Tipe Mobil</li>
+              <li><i class="icon-ok"></i> Antar Jemput Mobil</li>
+              <li><i class="icon-ok"></i> Mobil Nyaman dan Terawat</li>
+            </ul>
+          </div>
+          <div class="pricing-action">
+            <a href="https://api.whatsapp.com/send?phone=6285220489206.&text=Salam%20Saudara%20Trans.%20Saya%20ingin%20rental%20mobil%2012%20jam%20" class="img-responsive"><img src="img/pesan.png"></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3">
+        <div class="pricing-box-item">
+          <div class="pricing-heading">
+            <h3><strong>Rental 24 Jam</strong></h3>
+          </div>
+          <div class="pricing-terms">
+            <h6>Rp. 300.000,-</h6>
+          </div>
+          <div class="pricing-container">
+            <ul>
+              <li><i class="icon-ok"></i> Semua Tipe Mobil</li>
+              <li><i class="icon-ok"></i> Antar Jemput Mobil</li>
+              <li><i class="icon-ok"></i> Mobil Nyaman dan Terawat</li>
+            </ul>
+          </div>
+          <div class="pricing-action">
+            <a href="https://api.whatsapp.com/send?phone=6285220489206.&text=Salam%20Saudara%20Trans.%20Saya%20ingin%20rental%20mobil%2024%20jam%20" class="img-responsive"><img src="img/pesan.png"></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3">
+        <div class="pricing-box-item activeItem">
+          <div class="pricing-heading">
+            <h3><strong>Jasa Driver</strong></h3>
+          </div>
+          <div class="pricing-terms">
+            <h6>Rp. 150.000,-</h6>
+          </div>
+          <div class="pricing-container">
+            <ul>
+              <li><i class="icon-ok"></i> 12 Jam</li>
+              <li><i class="icon-ok"></i> Driver Ramah dan Berpengalaman</li>
+              <li><i class="icon-ok"></i> Antar Jemput Mobil</li>
+            </ul>
+          </div>
+          <div class="pricing-action">
+            <a href="https://api.whatsapp.com/send?phone=6285220489206.&text=Salam%20Saudara%20Trans.%20Saya%20ingin%20pesan%20jasa%20driver%2012%20jam%20" class="img-responsive"><img src="img/pesan.png"></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3">
+        <div class="pricing-box-item">
+          <div class="pricing-heading">
+            <h3><strong>Jasa Driver</strong></h3>
+          </div>
+          <div class="pricing-terms">
+            <h6>Rp. 200.000,-</h6>
+          </div>
+          <div class="pricing-container">
+            <ul>
+              <li><i class="icon-ok"></i> 12 Jam</li>
+              <li><i class="icon-ok"></i> Driver Ramah dan Berpengalaman</li>
+              <li><i class="icon-ok"></i> Antar Jemput Mobil</li>
+            </ul>
+          </div>
+          <div class="pricing-action">
+            <a href="https://api.whatsapp.com/send?phone=6285220489206.&text=Salam%20Saudara%20Trans.%20Saya%20ingin%20pesan%20jasa%20driver%2024%20jam%20" class="img-responsive"><img src="img/pesan.png"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+
+  <section id="content">
+  <div class="container">  
+    <!-- end divider -->
+    <div class="row">
+      <div class="col-lg-12">
+        <center><h2>HARGA ANTAR JEMPUT STASIUN DAN BANDARA SOETTA</h2></center>
+        <br>
+      </div>
+      <div class="col-lg-3">
+        <div class="pricing-box-item">
+          <div class="pricing-heading">
+            <h3><strong>Antar / Jemput</strong></h3>
+            <h3>Stasiun Cirebon</h3>
+          </div>
+          <div class="pricing-terms">
+            <h6>Rp. 300.000,-</h6>
+          </div>
+          <div class="pricing-container">
+            <ul>
+              <li><i class="icon-ok"></i> Mobil bersih, nyaman dan terawat</li>
+              <li><i class="icon-ok"></i> Driver ramah</li>
+              <li><i class="icon-ok"></i> Soft drink gratis</li>
+            </ul>
+          </div>
+          <div class="pricing-action">
+            <a href="https://api.whatsapp.com/send?phone=6285220489206.&text=Salam%20Saudara%20Trans.%20Saya%20ingin%20diantar%20atau%20jemput%20ke%20stasiun%20Cirebon%20" class="img-responsive"><img src="img/pesan.png"></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3">
+        <div class="pricing-box-item">
+          <div class="pricing-heading">
+            <h3><strong>Antar & Jemput</strong></h3>
+            <h3>Stasiun Cirebon</h3>
+          </div>
+          <div class="pricing-terms">
+            <h6>Rp. 400.000,-</h6>
+          </div>
+          <div class="pricing-container">
+            <ul>
+              <li><i class="icon-ok"></i> Mobil bersih, nyaman dan terawat</li>
+              <li><i class="icon-ok"></i> Driver ramah</li>
+              <li><i class="icon-ok"></i> Soft drink gratis</li>
+            </ul>
+          </div>
+          <div class="pricing-action">
+            <a href="https://api.whatsapp.com/send?phone=6285220489206.&text=Salam%20Saudara%20Trans.%20Saya%20ingin%20diantar%20dan%20jemput%20ke%20stasiun%20Cirebon%20" class="img-responsive"><img src="img/pesan.png"></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3">
+        <div class="pricing-box-item activeItem">
+          <div class="pricing-heading">
+            <h3><strong>Antar</strong></h3>
+            <h3>Bandara Soetta</h3>
+          </div>
+          <div class="pricing-terms">
+            <h6>Rp. 1.300.000,-</h6>
+          </div>
+          <div class="pricing-container">
+            <ul>
+              <li><i class="icon-ok"></i> Mobil bersih, nyaman dan terawat</li>
+              <li><i class="icon-ok"></i> Driver ramah</li>
+              <li><i class="icon-ok"></i> Soft drink gratis</li>
+            </ul>
+          </div>
+          <div class="pricing-action">
+            <a href="https://api.whatsapp.com/send?phone=6285220489206.&text=Salam%20Saudara%20Trans.%20Saya%20ingin%20diantar%20ke%20bandara%20Soetta%20" class="img-responsive"><img src="img/pesan.png"></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3">
+        <div class="pricing-box-item">
+          <div class="pricing-heading">
+            <h3><strong>Jemput</strong></h3>
+            <h3>Bandara Soetta</h3>
+          </div>
+          <div class="pricing-terms">
+            <h6>Rp. 1.300.000,-</h6>
+          </div>
+          <div class="pricing-container">
+            <ul>
+              <li><i class="icon-ok"></i> Mobil bersih, nyaman dan terawat</li>
+              <li><i class="icon-ok"></i> Driver ramah</li>
+              <li><i class="icon-ok"></i> Soft drink gratis</li>
+            </ul>
+          </div>
+          <div class="pricing-action">
+            <a href="https://api.whatsapp.com/send?phone=6285220489206.&text=Salam%20Saudara%20Trans.%20Saya%20ingin%20dijemput%20dari%20bandara%20Soetta%20" class="img-responsive"><img src="img/pesan.png"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+
+
+  <section id="content">  
   <div class="container">  
        <div class="row">
               <div class="col-md-7">
@@ -59,10 +248,8 @@
                 <p>SAUDARA TRANS memberikan fasilitas layanan yang lengkap mulai dari jenis mobil yang bervariasi, kondisi mobil yang bersih, nyaman dan terawat, layanan antar jemput mobil, serta supir yang ramah dan berpengalaman. <br><br>Armada yang tersedia lengkap meliputi mobil Avanza, Xenia, Livina, Luxio, Ayla, Calya, Toyota Hiace, dan Mobil bak terbuka.</p>
               </div>
               <div class="col-xs-12 col-sm-12 col-md-5">
-                 
                     <div class="latest-post-wrap pull-left"> 
                     <h3><span>GRATIS RENTAL</span></h3>
-                        
                         <!--Post item-->
                         <div class="post-item-wrap pull-left col-sm-6 col-md-12 col-xs-12">
                             <img src="img/service1.jpg" class="img-responsive post-author-img" alt="">
@@ -80,15 +267,10 @@
                          </div>
                         </div>
                     </div>
-  
-  
-              
             </div>
-  
     <div class="row">
       </div>
   </div>
-  
   </section>
   <div class="testimonial-area">
     <div class="testimonial-solid">
