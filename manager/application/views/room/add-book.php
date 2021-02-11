f07c717b80103d1327e781ae9a00d4d8c45ad688
Excellenz <63463920+jaruki2014@users.noreply.github.com>
2021-02-11 11:07:05 +0700

    Update add-book.php

--- manager/application/views/room/add-book.php
+++ manager/application/views/room/add-book.php
@@ -96,7 +96,7 @@
                         </div>
                       </div>
                       <div class="col-sm-6">
-                        <input class="form-control" name="waktu_checkin" value="14:00" readonly />
+                        <input class="form-control" name="waktu_checkin" value="Sesuai pengambilan mobil" readonly />
                       </div>
                     </div>
                   </div>
@@ -112,7 +112,7 @@
                         </div>
                       </div>
                       <div class="col-sm-6">
-                        <input class="form-control" name="waktu_checkout" value="12:00" readonly />
+                        <input class="form-control" name="waktu_checkout" value="Sesuai pengambilan mobil" readonly />
                       </div>
                     </div>
                   </div>
