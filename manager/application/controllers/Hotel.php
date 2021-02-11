0507e65dd56ae52b536c86aec8b0f9fb0d4ff46e
Excellenz <63463920+jaruki2014@users.noreply.github.com>
2021-02-11 10:29:39 +0700

    Update Hotel.php

--- manager/application/controllers/Hotel.php
+++ manager/application/controllers/Hotel.php
@@ -11,7 +11,7 @@ class Hotel extends CI_Controller
 
 	public function index()
 	{
-		$data['title'] = 'Hotel Dashboard';
+		$data['title'] = 'Rental Dashboard';
 		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
 		$this->load->view('templates/header', $data);
